5156b436e78e14c66520ca0a30d1a2539b17c02d
Sakshi Wankhede <sakwan132000@gmail.com>
2023-12-18 18:10:56 +0530

    newchange

--- src/videoplayer/videoHeaderNew.jsx
+++ src/videoplayer/videoHeaderNew.jsx
@@ -4,10 +4,11 @@ import Avatar from "antd/es/avatar/avatar";
 import { UserOutlined, DownOutlined } from "@ant-design/icons";
 import { useCookies } from "react-cookie";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 export function Header(props) {
   const [cookies, setcookies, removecookies] = useCookies();
   const [searchvalue, setsearchvalue] = useState("");
+  const [updatesearchvalue, setupdatesearchvalue] = useState("");
   const items = [
     {
       label: (
@@ -18,10 +19,20 @@ export function Header(props) {
       key: 1,
     },
   ];
+  // const submithandler = (e) => {
+  //   e.preventDefault();
+  //   console.log("searchvalue", searchvalue);
+  //   let newFilterVideo = props.state.video.filter((items) => {
+  //     //   console.log("RRRRR items", items.title);
+  //     return searchvalue.toLowerCase() === items.title.toLowerCase();
+  //   });
+  //   props.setState({ video: newFilterVideo });
+  //   // setsearchvalue = "";
+  // };
   const submithandler = (e) => {
     e.preventDefault();
-    // console.log("searchvalue", searchvalue);
-    let newFilterVideo = props.state.video.filter((items) => {
+    console.log("searchvalue", searchvalue);
+    var newFilterVideo = props.state.video.filter((items) => {
       //   console.log("RRRRR items", items.title);
       return searchvalue.toLowerCase() === items.title.toLowerCase();
     });

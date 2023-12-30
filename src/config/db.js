0e11d30737751dbd5143478f8d2248b0cdf0fcaa
Sakshi Wankhede <sakwan132000@gmail.com>
2023-12-30 17:18:48 +0530

    added mongoos connection

---
+++ src/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const DB_URI = "mongodb+srv://sakshi13:Sakshi%40100@cluster0.22zds3k.mongodb.net/";
+const connectionParams = {
+  useNewUrlParser: true,
+//   useCreateIndex: true,
+  useUnifiedTopology: true,
+};
+const connectDB = async () => {
+  try {
+    await mongoose.connect(DB_URI, connectionParams);
+    console.log("database connect");
+  } catch (error) {
+    console.log("erroe in connection" + error.message);
+  }
+};
+module.exports = connectDB;

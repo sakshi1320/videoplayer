648f27f36ecaee6e0adf976919dfce275fa45497
Sakshi Wankhede <sakwan132000@gmail.com>
2024-01-02 14:03:23 +0530

    uri changed

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
-const DB_URI = "mongodb+srv://sakshi13:Sakshi%40100@cluster0.22zds3k.mongodb.net/";
+const DB_URI = "mongodb+srv://sakshi13:Sakshi%40100@cluster0.22zds3k.mongodb.net/videoplayer";
 const connectionParams = {
   useNewUrlParser: true,
-//   useCreateIndex: true,
+  //   useCreateIndex: true,
   useUnifiedTopology: true,
 };
 const connectDB = async () => {
